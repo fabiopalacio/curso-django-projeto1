3cc0a2678dbf655cbdf14c075673eb19a0ef43c5
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-05-03 19:57:48 -0300

    The Recipe and Category models were created in the models.py file, they were registered in the admin.py file, and the models were migrated. The project's settings.py and urls.py files were edited to find the media urls.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Category, Recipe
 # Register your models here.
+# Two ways to register a model. Using the admin.site.register(ClassImported, ClassAdminCreatedHere) and
+# using the @admin.register(ClassImported) before the creation of the ClassAdmin.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
