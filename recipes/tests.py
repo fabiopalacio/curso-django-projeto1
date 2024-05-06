29232918d1cc4b45832e679c1b089477a39e16ab
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-05-06 09:35:03 -0300

    Initial configuration to pytest

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase  # type: ignore
 
 # Create your tests here.
+
+
+# Testing if the test configuration in the pytest.ini file is correct
+class RecipeURLsTest(TestCase):
+
+    def test_the_pytest_is_ok(self):
+        print('Olá')
+        ...
