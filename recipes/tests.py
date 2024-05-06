48264bbcef142280755c6ad5b1351c89a843a4f7
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-05-06 19:54:34 -0300

    The RecipeURLsTest class was finished.

--- recipes/tests.py
+++ recipes/tests.py
@@ -6,6 +6,14 @@ from django.urls import reverse  # type: ignore
 class RecipeURLsTest(TestCase):
 
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
+        url = reverse('recipes:home')
         # Checking if the recipes:home url is '/'
-        self.assertEqual(home_url, '/')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
