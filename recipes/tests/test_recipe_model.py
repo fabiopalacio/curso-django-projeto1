316b05320a718ce7fc670d8d63497865f5163018
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-05-08 10:48:25 -0300

    The requirements.txt file was updated. The test to check if the max_length from the Recipe model is ok was created.

---
+++ recipes/tests/test_recipe_model.py
@@ -0,0 +1,21 @@
+from .test_recipe_base import RecipeTestBase
+from django.core.exceptions import ValidationError  # type: ignore
+from parameterized import parameterized  # type: ignore
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65)
+    ])
+    def test_recipe_fiels_max_len(self, field, max_length):
+
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
