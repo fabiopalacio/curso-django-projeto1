87f3cfbde0f80e4e180bbe1d034e59784883583c
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-05-21 12:37:42 -0300

    Tests to login were created

--- authors/tests/test_authors_login_view.py
+++ authors/tests/test_authors_login_view.py
@@ -2,14 +2,62 @@
 from django.test import TestCase
 from django.urls import reverse
 
+# CLASS to test login View
+
 
 class LoginViewTest(TestCase):
+    # TEST if login_auth returns 404  to GET method
     def test_login_view_returns_404(self):
+        # Getting the url to login_auth
         url = reverse('authors:login_auth')
+
+        # Getting the response to GET the url
         response = self.client.get(url)
+
+        # Assertions:
+        # Check the status code returned
         self.assertEqual(
             404,
             response.status_code,
             msg="AUTHORS LOGIN_AUTH VIEW - GET VIEW: Get request did NOT "
             "returned 404 status code. Expected code: 404. Found: "
             f"{response.status_code}")
+
+    # TEST if view return error message to invalid form
+
+    def test_login_view_invalid_form_return_error_message(self):
+        # Creating invalid form with no username inserted
+        form_data = {'username': '', 'password': 'Password123'}
+
+        # Posting the form to the view and saving it response.
+        response = self.client.post(
+            '/authors/login/auth/', form_data, follow=True)
+
+        # Assertions:
+        # Checking if the error message was displayed to the user
+        self.assertIn(
+            'class="message message-error">\n                '
+            'Invalid credentials.',
+            response.content.decode('utf-8'),
+            msg='LOGIN_AUTH - CREDENTIALS: Expected error message after '
+            'inserting invalid credentials.'
+        )
+
+    # TEST if the view returns a warning message to unauthenticated user
+    def test_login_view_unauthenticate_form_return_warning(self):
+        # Creating a valid form, but invalid user
+        form_data = {'username': '123456', 'password': 'Password123'}
+
+        # Posting the form and saving the response
+        response = self.client.post(
+            '/authors/login/auth/', form_data, follow=True)
+
+        # Assertios:
+        # Check if the WARNING message was displayed to the user
+        self.assertIn(
+            'class="message message-warning">\n                '
+            'Invalid credentials.',
+            response.content.decode('utf-8'),
+            msg='LOGIN_AUTH - CREDENTIALS: Expected warning message after '
+            'inserting invalid credentials.'
+        )
--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -6,41 +6,124 @@ from selenium.webdriver.common.by import By
 
 from .base import AuthorsBaseTest
 
+# CLASS to functional tests to login views
+
 
 @pytest.mark.functionaltest
 class AuthorsLoginTest(AuthorsBaseTest):
+
+    # Test if a valid user can login
     def test_user_valid_data_can_login_successfully(self):
+
+        # Saving the password to be used in multiple points
+        # in this test
         string_password = 'MyPassword1234'
 
+        # Creating an user
         user = User.objects.create_user(
             username='my_user',
             password=string_password)
 
+        # Acessing the login page
         self.browser.get(self.live_server_url + reverse('authors:login'))
 
+        # Getting the login form
         form = self.browser.find_element(By.CLASS_NAME, 'main-form')
 
+        # Inserting the username and password
         username_field = form.find_element(By.ID, 'id_username')
         username_field.send_keys(user.username)
 
         password_field = form.find_element(By.ID, 'id_password')
         password_field.send_keys(string_password)
 
+        # Submitting the form
         form.submit()
+
+        # Waiting the page to load
         self.sleep(1)
 
+        # Assertions:
+        # Check if the successfull login message was displayed to the user
         self.assertIn(
             'You are logged in.',
             self.browser.find_element(By.TAG_NAME, 'body').text,
             msg="AUTHORS:LOGIN - VALID USER: User wasn't accepted to log in "
             "with valid data. Success message wasn't found in the body.")
 
+    # TEST if GET method to login_auth returns 404
     def test_get_login_auth_view_returns_404(self):
 
+        # Getting the page
         self.browser.get(self.live_server_url + reverse('authors:login_auth'))
 
+        # Assertions:
+        # Check if Not Found message was found in the returned page.
         self.assertIn(
             'Not Found',
             self.browser.find_element(By.TAG_NAME, 'body').text,
             msg='Deu Ruim'
+        )
+
+    # TEST if an invalid form returns an error message
+    def test_login_auth_error_message_to_invalid_form(self):
+        # Accessing the page
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+
+        # Getting the form
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # Inserting only space in username
+        # This makes the form invalid
+        username_field = self.get_by_id(form, 'id_username')
+        password_field = self.get_by_id(form, 'id_password')
+
+        username_field.send_keys(' ')
+        password_field.send_keys('Apassword123')
+
+        # Submitting the form
+        form.submit()
+
+        # Waiting the page reload
+        self.sleep(1)
+
+        # Assertions:
+        # Check if the "Invalid credentials." was displayed as an error
+        # message to the user
+        self.assertIn(
+            'Invalid credentials.',
+            self.browser.find_element(By.CLASS_NAME, 'message-error').text,
+            msg="AUTHORS:LOGIN - CREDENTIALS: Error message to invalid "
+            "credentials not found"
+        )
+
+    # TEST if an unauthenticated user returns warning message
+    def test_login_auth_warning_message_to_invalid_user(self):
+        # Accessing the page
+        self.browser.get(self.live_server_url + reverse('authors:login'))
+        # Getting the form
+        form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+        # Inserting username and password valids, but not
+        # registered.
+        username_field = self.get_by_id(form, 'id_username')
+        password_field = self.get_by_id(form, 'id_password')
+
+        username_field.send_keys('123username')
+        password_field.send_keys('Apassword123')
+
+        # Submitting the form
+        form.submit()
+
+        # Waiting the page reload
+        self.sleep(1)
+
+        # Assertions:
+        # Check if the "Invalid credentials." was displayed as warning
+        # message to the user
+        self.assertIn(
+            'Invalid credentials.',
+            self.browser.find_element(By.CLASS_NAME, 'message-warning').text,
+            msg="AUTHORS:LOGIN - CREDENTIALS: Warning message to invalid "
+            "credentials not found"
         )
