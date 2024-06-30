0952df4d8c8f7141cd3452eeb2afe29ebabc494f
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-06-30 17:47:07 -0300

    Main code to run environment removed. Only methods keeped.

--- utils/environment.py
+++ utils/environment.py
@@ -10,12 +10,3 @@ def parse_comma_sep_to_list(comma_sep_str):
     if not comma_sep_str or not isinstance(comma_sep_str, str):
         return []
     return [string.strip() for string in comma_sep_str.split(',') if string]
-
-
-if __name__ == 'main':
-    from dotenv import load_dotenv
-    load_dotenv()
-    print(parse_comma_sep_to_list(get_env_variables('ALLOWED_HOSTS')))
-    print(parse_comma_sep_to_list(''))
-    print(parse_comma_sep_to_list(123))
-    print(parse_comma_sep_to_list('a,b,c'))
