292b4d16b4ef46770c3b4535c2c6595353d8cafe
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-07-01 18:20:36 -0300

    EventListener to open and close side menu

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,4 +1,4 @@
-function my_scope() {
+(() => {
   const forms = document.querySelectorAll(".form-delete");
 
   for (const form of forms) {
@@ -11,6 +11,33 @@ function my_scope() {
       }
     });
   }
-}
 
-my_scope();
+  (() => {
+    const buttonCloseMenu = document.querySelector(".button-close-menu");
+    const buttonShowMenu = document.querySelector(".button-show-menu");
+    const menuContainer = document.querySelector(".menu-container");
+
+    const buttonShowMenuVisibleClass = "button-show-menu-visible";
+    const menuHiddenClass = "menu-hidden";
+
+    const showMenu = () => {
+      menuContainer.classList.remove(menuHiddenClass);
+      buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    };
+
+    const closeMenu = () => {
+      menuContainer.classList.add(menuHiddenClass);
+      buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    };
+
+    if (buttonCloseMenu) {
+      buttonCloseMenu.removeEventListener("click", closeMenu);
+      buttonCloseMenu.addEventListener("click", closeMenu);
+    }
+
+    if (buttonShowMenu) {
+      buttonShowMenu.removeEventListener("click", showMenu);
+      buttonShowMenu.addEventListener("click", showMenu);
+    }
+  })();
+})();
