d52d6c7e43df3915d40b75692216e62a494a66f4
Fabio Bignoto Palacio <fabiopalacio14@gmail.com>
2024-07-04 08:34:51 -0300

    Signals to remove old images when cover image is updated

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,6 +1,6 @@
 import os
 
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, pre_save, post_save
 from django.dispatch import receiver
 
 from recipes.models import Recipe
@@ -17,4 +17,20 @@ def delete_cover(instance):
 @receiver(pre_delete, sender=Recipe)
 def recipe_cover_delete(sender, instance, *args, **kwargs):
     old_instance = Recipe.objects.get(pk=instance.pk)
-    delete_cover(old_instance)
+    if instance.cover.name:
+        delete_cover(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update_pre_save(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk).first()
+    if old_instance:
+        is_new_cover = old_instance.cover != instance.cover
+        if is_new_cover:
+            instance.old_instance = old_instance
+
+
+@receiver(post_save, sender=Recipe)
+def recipe_cover_update_post_save(sender, instance, *args, **kwargs):
+    if instance.old_instance:
+        delete_cover(instance.old_instance)
